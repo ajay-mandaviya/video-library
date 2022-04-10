ab5b28b493e0812b2f9377459544058b32f1730e
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2022-04-10 17:23:37 +0530

    fix : review suggestion

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,61 +2,39 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./sidebar.css";
 const Sidebar = () => {
+  const getActiveStyle = ({ isActive }) => {
+    return isActive ? "sidebar-link active" : "sidebar-link";
+  };
   return (
     <div className="sidebar-container">
       <ul>
         <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? "sidebar-link active" : "sidebar-link"
-            }
-          >
+          <NavLink to="/" className={getActiveStyle}>
             <i className="fa fa-home" aria-hidden="true"></i>
             <div>Home</div>
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/playlist"
-            className={({ isActive }) =>
-              isActive ? "sidebar-link active" : "sidebar-link"
-            }
-          >
+          <NavLink to="/playlist" className={getActiveStyle}>
             <i className="fas fa-play-circle"></i>
             <div>PlayList</div>
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/liked"
-            className={({ isActive }) =>
-              isActive ? "sidebar-link active" : "sidebar-link"
-            }
-          >
+          <NavLink to="/liked" className={getActiveStyle}>
             <i className="fas fa-thumbs-up"></i>
 
             <div>Liked</div>
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/watchLater"
-            className={({ isActive }) =>
-              isActive ? "sidebar-link active" : "sidebar-link"
-            }
-          >
+          <NavLink to="/watchLater" className={getActiveStyle}>
             <i className="fas fa-clock"></i>
             <div>Watch Later</div>
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/history"
-            className={({ isActive }) =>
-              isActive ? "sidebar-link active" : "sidebar-link"
-            }
-          >
+          <NavLink to="/history" className={getActiveStyle}>
             <i className="fas fa-history"></i>
             <div>History</div>
           </NavLink>
